198650121579941cb9f784e60e8772d0b2649b69
Julius Kakai <cjkakai5@gmail.com>
2025-06-13 19:24:19 +0300

    add html and js file

---
+++ index.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('guest-form');
+const guestInput = document.getElementById('guest-name');
+const guestList = document.getElementById('guest-list')
+let guestCount =0;
+const maxGuest = 10;
+
+//stop form from submitting the normal way
+form.addEventListener('submit',function(e){
+e.preventDefault();
+
+//get the trimmed name from the input
+const name = guestInput.value.trim();
+
+//prevent empty name or if guest limit is reached
+if (name === "")return;
+if(guestCount >= maxGuest){
+    alert("Guest limit reached")
+    return;
+}
+
+//create a new list with guest name
+const li = document.createElement('li');
+li.innerHTML = 
+`<span class="guest-name">${name}</span>
+<button class="toggle-rsvp">Not attending<button/>
+<button class="remove">Remove<button/>
+<button class="edit">Edit</button>
+`;
+guestList.appendChild(li);
+guestCount++;
+guestInput.value = ""
+//remove button for each guest
+ li.querySelector(".remove").addEventListener("click", () => {
+    li.remove();
+    guestCount--;
+  })
+  li.querySelector(".toggle-rsvp").addEventListener("click", (e) => {
+    e.target.textContent =
+      e.target.textContent === "Attending" ? "Not Attending" : "Attending";
+    li.classList.toggle("attending");})
+    li.querySelector(".edit").addEventListener("click", () =>
+    {
+     const newName = prompt("Update guest name:, name");   
+     if (newName) li.querySelector(".guest-name").textContent = newName;
+    })
+})
